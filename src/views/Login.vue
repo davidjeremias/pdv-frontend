e5b886a96d4f240214aeb82b522f7a25872e9613
p_917520 <david.carmo@camara.leg.br>
2020-01-06 17:45:26 -0200

    [Update] botao de logar

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,15 +3,15 @@
         <form autocomplete="off">
             <Panel header="Login">
                 <span class="p-float-label">
-                    <InputText id="username" type="text"/>
+                    <InputText id="username" type="text" v-model="user"/>
                     <label for="username">Username</label>
                 </span>
 
                 <span class="p-float-label">
-                    <InputText id="password" type="password"/>
+                    <InputText id="password" type="password" v-model="password"/>
                     <label for="password">Password</label>
                 </span>
-                <Button label="Primary" />
+                <Button label="Primary" @click="logar()"/>
             </Panel>
             
         </form> 
@@ -20,7 +20,20 @@
 
 <script>
 export default {
-    name: 'Login'
+    name: 'Login',
+    data(){
+        return{
+            user: '',
+            password: ''
+        }
+    },
+    methods:{
+        logar(){
+            if(this.user !== null && this.password !== null){
+                this.$router.push({name: 'home'})
+            }
+        }
+    }
 }
 </script>
 
